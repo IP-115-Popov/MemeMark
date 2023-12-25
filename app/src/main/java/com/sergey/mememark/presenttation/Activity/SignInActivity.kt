07b5a67de8189efb2cf5d0a2ad9c7518a91fdb85
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-12-25 18:54:40 +0700

    SignIn marking

---
+++ app/src/main/java/com/sergey/mememark/presenttation/Activity/SignInActivity.kt
@@ -0,0 +1,18 @@
+package com.sergey.mememark.presenttation.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sergey.mememark.R
+import com.sergey.mememark.databinding.ActivityMainBinding
+import com.sergey.mememark.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+    private var _binding: ActivitySignInBinding? = null
+    private val binding: ActivitySignInBinding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityMain must not be null")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
